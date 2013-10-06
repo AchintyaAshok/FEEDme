db2d7e2938d0961afda20a7db19dee7e4ebfe44c
Dibyo Mukherkee <dibyo.m@gmail.com>
2013-10-06 02:21:09 -0400

    Add index and create for table_items

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime
-#  updated_at :datetime
+#  id            :integer          not null, primary key
+#  created_at    :datetime
+#  updated_at    :datetime
+#  venmo_user_id :string(255)
 #
 
 class User < ActiveRecord::Base
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131006030313) do
+ActiveRecord::Schema.define(version: 20131006053825) do
 
   create_table "restaurant_table_users", force: true do |t|
     t.integer  "user_id"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 20131006030313) do
   create_table "restaurant_tables", force: true do |t|
     t.string   "venue_locu_id", null: false
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "table_items", force: true do |t|
+    t.integer  "table_id"
+    t.string   "item_name"
+    t.integer  "quantity"
+    t.float    "price"
+    t.boolean  "paid"
+    t.integer  "paying_user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
