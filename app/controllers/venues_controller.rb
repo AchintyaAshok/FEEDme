4026278c2b5cdef7b8049986917b401b8f5c17b3
Dibyo Mukherkee <dibyo.m@gmail.com>
2013-10-05 20:54:59 -0400

    Add search functionality

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,7 +2,7 @@ class VenuesController < ActionController::Base
 	
 	#Search endpoint. Input: a restaurant name, 
 	def index
-		search_term = "sea+food"
+		search_term = params["venue"].tr(" ", "+")
 		url = "http://api.locu.com/v1_0/venue/search/?name=#{search_term}&has_menu=true&category=restaurant&locality=Boston&api_key=c441cb7b1b3f83a2644a6bc573dd8ebf3e9a1afb"
 		venue = HTTParty.get(url)
 		if venue
