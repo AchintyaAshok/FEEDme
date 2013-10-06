795605ad1f9c38e877b72492651fc394df1e119b
Dibyo Mukherkee <dibyo.m@gmail.com>
2013-10-05 23:46:45 -0400

    Skip authenticity token for update

--- app/controllers/restaurant_tables_controller.rb
+++ app/controllers/restaurant_tables_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantTablesController < ApplicationController
-	skip_before_filter :verify_authenticity_token, only: :create	
+	skip_before_filter :verify_authenticity_token, only: [:create, :update]
 	respond_to :json
 
 	def index
